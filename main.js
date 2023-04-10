3fe307dc3528d773f8132594a7b30b958e773195
Alexandre Artisien | RouleMarcel <alexandre.artisien@gmail.com>
2023-04-10 17:38:40 +0200

    initialisation du projet

---
+++ main.js
@@ -0,0 +1,93 @@
+import * as THREE from "three";
+
+import Stats from "three/addons/libs/stats.module.js";
+
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { RoomEnvironment } from "three/addons/environments/RoomEnvironment.js";
+
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
+
+let mixer;
+
+const clock = new THREE.Clock();
+const container = document.getElementById("container");
+console.log(container);
+
+const stats = new Stats();
+// container.appendChild(stats.dom);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(container.devicePixelRatio);
+renderer.setSize(container.clientWidth, container.clientHeight);
+renderer.outputEncoding = THREE.sRGBEncoding;
+container.appendChild(renderer.domElement);
+
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
+scene.environment = pmremGenerator.fromScene(
+  new RoomEnvironment(),
+  0.04
+).texture;
+
+const camera = new THREE.PerspectiveCamera(
+  40,
+  container.clientWidth / container.clientHeight,
+  1,
+  100
+);
+camera.position.set(0, 0, 5);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0.5, 0);
+controls.update();
+controls.enablePan = false;
+controls.enableDamping = true;
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("jsm/libs/draco/gltf/");
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
+loader.load(
+  "assets/scene.gltf",
+  function (gltf) {
+    const model = gltf.scene;
+    model.position.set(0, 0, 0);
+    model.scale.set(1, 1, 1);
+    scene.add(model);
+
+    // rendre les possibles annimations du modèle 3D
+    mixer = new THREE.AnimationMixer(model);
+    mixer.clipAction(gltf.animations[0]).play();
+
+    animate();
+  },
+  undefined,
+  function (e) {
+    console.error(e);
+  }
+);
+
+window.onresize = function () {
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+};
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  const delta = clock.getDelta();
+
+  mixer.update(delta);
+
+  controls.update();
+
+  stats.update();
+
+  renderer.render(scene, camera);
+}
